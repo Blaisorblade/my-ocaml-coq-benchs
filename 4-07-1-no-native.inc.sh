c0229f4c1c273d7215967958e5af8801194d7f5c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-11-21 16:36:25 +0100

    New config

---
+++ 4-07-1-no-native.inc.sh
@@ -0,0 +1,5 @@
+# Hey Emacs, this file needs -*- sh -*- mode
+switchName=ocaml-system-coq-8.10.0-no-native
+compilerVersion=ocaml-base-compiler.4.07.1
+
+# vim: ft=sh sw=2
---
+++ bench-4-07-1-no-native.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. funcs.inc.sh
+. 4-07-1-no-native.inc.sh
+
+bench
