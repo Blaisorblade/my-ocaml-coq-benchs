110642727ffa7ba53e820c47539e9ee8d9657a1d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-11-21 16:45:23 +0100

    More uniform compiler choices

--- 4-09-0-flambda.inc.sh
+++ 4-09-0-flambda.inc.sh
@@ -1,5 +1,5 @@
 # Hey Emacs, this file needs -*- sh -*- mode
 switchName=ocaml4.09.0-flambda-coq-8.10.0
-compilerVersion=4.09.0+flambda+no-flat-float-array
+compilerVersion=4.09.0+flambda
 
 # vim: ft=sh sw=2
--- 4-09-0.inc.sh
+++ 4-09-0.inc.sh
@@ -1,5 +1,5 @@
 # Hey Emacs, this file needs -*- sh -*- mode
 switchName=ocaml4.09.0-coq-8.10.0
-compilerVersion=ocaml-base-compiler.4.09.0
+compilerVersion=4.09.0
 
 # vim: ft=sh sw=2
