c24f1cb4819522cdff388102d78ed005a723a64a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-11-25 14:39:36 +0100

    Add 4-07-1-no-native config

---
+++ 4-07-1-no-native.inc.sh
@@ -0,0 +1,5 @@
+# Hey Emacs, this file needs -*- sh -*- mode
+compilerVersion=4.07.1
+coqVersion=8.10.1-no-native
+
+# vim: ft=sh sw=2
---
+++ create-4-07-1-no-native.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. 4-07-1-no-native.inc.sh
+. funcs.inc.sh
+
+setup
