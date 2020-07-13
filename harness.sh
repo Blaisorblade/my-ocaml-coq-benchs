5d3e566f8dd0ebe143190191dea103cc000ee646
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-07-13 22:51:25 +0200

    Update harness.sh for the interesting configurations

--- harness.sh
+++ harness.sh
@@ -1,31 +1,25 @@
 #!/bin/bash -x
 
 ./create-4-07-1-flambda-no-native.sh
-./create-4-07-1-flambda.sh
-./create-4-07-1.sh
+./create-4-07-1-no-native.sh
 ./create-4-10-0-flambda-no-native.sh
-./create-4-10-0-flambda.sh
-./create-4-10-0.sh
+./create-4-10-0-no-native.sh
 
 reps=3
 flambdaThenNorm() {
   for i in `seq 1 $reps`; do
     ./bench-4-10-0-flambda-no-native.sh
-    ./bench-4-10-0-flambda.sh
-    ./bench-4-10-0.sh
+    ./bench-4-10-0-no-native.sh
     ./bench-4-07-1-flambda-no-native.sh
-    ./bench-4-07-1-flambda.sh
-    ./bench-4-07-1.sh
+    ./bench-4-07-1-no-native.sh
   done
 }
 normThenFlambda() {
   for i in `seq 1 $reps`; do
-    ./bench-4-10-0.sh
+    ./bench-4-10-0-no-native.sh
     ./bench-4-10-0-flambda-no-native.sh
-    ./bench-4-10-0-flambda.sh
-    ./bench-4-07-1.sh
+    ./bench-4-07-1-no-native.sh
     ./bench-4-07-1-flambda-no-native.sh
-    ./bench-4-07-1-flambda.sh
   done
 }
 flambdaThenNorm
