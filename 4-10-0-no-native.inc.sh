7713712e0c5390dfdca3be17efe0157bb0e85e32
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-07-13 22:51:09 +0200

    Add newly interesting configuration

---
+++ 4-10-0-no-native.inc.sh
@@ -0,0 +1,5 @@
+# Hey Emacs, this file needs -*- sh -*- mode
+compilerVersion=4.10.0
+coqVersion=8.10.1-native
+
+# vim: ft=sh sw=2
---
+++ bench-4-10-0-no-native.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. 4-10-0-no-native.inc.sh
+. funcs.inc.sh
+
+bench
