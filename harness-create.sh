f1fc4b196b36e7edfb61d3d94c966ed21c3bdef9
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-11-07 15:25:16 +0100

    Restrict to flambda

--- harness-create.sh
+++ harness-create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 ./4-07-1/create-flambda-no-native.sh
-./4-07-1/create-no-native.sh
+#./4-07-1/create-no-native.sh
 ./4-10-2/create-flambda-no-native.sh
-./4-10-2/create-no-native.sh
+#./4-10-2/create-no-native.sh
 ./4-12-0/create-flambda-no-native.sh
-./4-12-0/create-no-native.sh
+#./4-12-0/create-no-native.sh
