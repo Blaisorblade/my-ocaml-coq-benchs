242955331407942f95b8ef16486ce70e8f78ff78
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-11-07 13:54:42 +0100

    4.10.0 -> 4.10.2 part 1
    Done with:
    ```
    gsed -i'' -e 's/4-10-0/4-10-2/g' *
    ```

--- harness-create.sh
+++ harness-create.sh
@@ -2,5 +2,5 @@
 
 ./create-4-07-1-flambda-no-native.sh
 ./create-4-07-1-no-native.sh
-./create-4-10-0-flambda-no-native.sh
-./create-4-10-0-no-native.sh
+./create-4-10-2-flambda-no-native.sh
+./create-4-10-2-no-native.sh
--- harness.sh
+++ harness.sh
@@ -3,16 +3,16 @@
 reps=3
 flambdaThenNorm() {
   for i in `seq 1 $reps`; do
-    ./bench-4-10-0-flambda-no-native.sh
-    ./bench-4-10-0-no-native.sh
+    ./bench-4-10-2-flambda-no-native.sh
+    ./bench-4-10-2-no-native.sh
     ./bench-4-07-1-flambda-no-native.sh
     ./bench-4-07-1-no-native.sh
   done
 }
 normThenFlambda() {
   for i in `seq 1 $reps`; do
-    ./bench-4-10-0-no-native.sh
-    ./bench-4-10-0-flambda-no-native.sh
+    ./bench-4-10-2-no-native.sh
+    ./bench-4-10-2-flambda-no-native.sh
     ./bench-4-07-1-no-native.sh
     ./bench-4-07-1-flambda-no-native.sh
   done
