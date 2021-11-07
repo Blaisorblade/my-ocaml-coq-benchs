609f194b9a2494ba2f9666f56aef620cdf1db460
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-11-07 13:55:24 +0100

    4.10.0 -> 4.10.2 part 2
    ```
    gsed -i'' -e 's/4.10.0/4.10.2/g' *
    ```

--- 4-10-0-flambda-no-native.inc.sh
+++ 4-10-0-flambda-no-native.inc.sh
@@ -1,4 +1,4 @@
 # Hey Emacs, this file needs -*- sh -*- mode
-compilerVersion=4.10.0+flambda
+compilerVersion=4.10.2+flambda
 
 # vim: ft=sh sw=2
--- 4-10-0-no-native.inc.sh
+++ 4-10-0-no-native.inc.sh
@@ -1,4 +1,4 @@
 # Hey Emacs, this file needs -*- sh -*- mode
-compilerVersion=4.10.0
+compilerVersion=4.10.2
 
 # vim: ft=sh sw=2
