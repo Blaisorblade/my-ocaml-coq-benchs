a2a9b001dfbf3ce6cfdd76a51cc011fe6a1e2720
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-11-07 14:34:08 +0100

    Copy 4-10-2 to 4-12-0

---
+++ 4-12-0/bench-flambda-no-native.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. 4-10-2/flambda-no-native.inc.sh
+. funcs.inc.sh
+
+bench
---
+++ 4-12-0/flambda-no-native.inc.sh
@@ -0,0 +1,4 @@
+# Hey Emacs, this file needs -*- sh -*- mode
+compilerVersion=4.10.2+flambda
+
+# vim: ft=sh sw=2
